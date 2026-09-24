fdaa8a8bdaa3be0c523ef5730db41eb01c5b14de
jeanollion <smogollion@yahoo.fr>
2026-09-24 17:08:07 +0200

    v0.1.7

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PixMClass",
-    version="0.1.6",
+    version="0.1.7",
     author="Jean Ollion",
     author_email="jean.ollion@sabilab.fr",
     description="Multiclass pixel classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeanollion/pix_mclass",
-    download_url='https://github.com/jeanollion/pix_mclass/releases/download/v0.1.6/pix_mclass-0.1.6.tar.gz',
+    download_url='https://github.com/jeanollion/pix_mclass/releases/download/v0.1.7/pix_mclass-0.1.7.tar.gz',
     packages=setuptools.find_packages(),
     keywords = ['Segmentation', 'Classification', 'Microscopy', 'Cell'],
     classifiers=[
